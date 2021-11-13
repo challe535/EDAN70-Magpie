fef5ead5ad946c99c4f189ae2708f16dd5de2ae7
challe535 <31891184+challe535@users.noreply.github.com>
2021-11-13 18:59:10 +0100

    Fixed automatic vsce build script

--- generate_vsce.bat
+++ generate_vsce.bat
@@ -1,6 +1,12 @@
 @REM Requires npm installed to run
+
+call @echo off
+
 call cd vscode
-call npm list || npm install
-call vsce package || (npm install -g vsce && vsce package)
-call code --install-extension intrep-0.0.1.vsix || (code --uninstall-extension intrep-0.0.1.vsix && Taskkill /IM code.exe /F && code -r && code --install-extension intrep-0.0.1.vsix)
+call npm list || call npm install
+call vsce package || (call npm install -g vsce && call vsce package)
+call code --uninstall-extension intrep-0.0.1.vsix 
+Taskkill /IM code.exe /F
+call code --install-extension intrep-0.0.1.vsix
+call code -r
 PAUSE
